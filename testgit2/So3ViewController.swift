64a2e40086e623c8fc82f550ea58f8e061ed1622
Qduk22 <146013823+Qduk22@users.noreply.github.com>
2025-05-22 17:37:03 +0700

    them so 3 4

---
+++ testgit2/So3ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  So3ViewController.swift
+//  testgit2
+//
+//  Created by Dukkk on 22/05/2025.
+//
+
+import UIKit
+
+class So3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+print("so3")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
