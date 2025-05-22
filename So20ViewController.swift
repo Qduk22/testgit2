59b86c2cdc556a0ff411ab4ab0bf42cda3b7103f
Qduk22 <146013823+Qduk22@users.noreply.github.com>
2025-05-22 21:51:23 +0700

    Create So20ViewController.swift

---
+++ So20ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  So12ViewController.swift
+//  testgit2
+//
+//  Created by Dukkk on 22/05/2025.
+//
+
+import UIKit
+
+class So20ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+print("so20")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
